b7d87c2a5dd331ba935fac97a5d156c72733f9c7
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-06-27 20:40:45 +0530

    adding modal via portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,27 @@
-import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
 
-const Cart = props => {
-    
-    const cartItems = <ul className={classes['cart-item']}>{
-    [{id: 'c1',name: 'Sushi', amount: 2, price : 12.23} 
-    ].map(item => <li>{item.name}</li>)}</ul>
-    return <div>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>35.63</span>
-        </div>
-        <div className={classes.action}>
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes.button}>Oreder</button>
-        </div>
-    </div>
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.23 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.63</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Oreder</button>
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
