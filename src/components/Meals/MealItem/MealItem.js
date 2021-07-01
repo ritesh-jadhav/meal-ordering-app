09e91aa96e9f81612d2cacd28d56170ed4add23f
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-01 11:28:10 +0530

    updating cart number on selected quantity

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,20 @@
+import {useContext} from 'react';
 import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const addItemToCartHandler= amount => {
+    cartCtx.addItem({
+      id : props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    })
+  };
+
   const price = `$${props.price.toFixed(2)}`;
 
   return (
@@ -12,7 +25,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addItemToCartHandler}/>
       </div>
     </li>
   );
